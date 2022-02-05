3da483e09dd2df3b756005f5b347338ba5efcdd9
Elijah0tieno <eliasotieno99@gmail.com>
2022-02-06 00:07:34 +0300

    Added database models

--- church/admin.py
+++ church/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Department, Family, Announcement, Member
 
 # Register your models here.
+admin.site.register(Department)
+admin.site.register(Family)
+admin.site.register(Announcement)
+admin.site.register(Member)
--- church/models.py
+++ church/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+class Department(models.Model):
+    name = models.CharField(max_length=250, null=False)
+    description = models.CharField(max_length=250)
+    head = models.CharField(max_length=250)
+    assistant = models.CharField(max_length=250)
+    
+    def __str__(self):
+        return self.name
+
+class Family(models.Model):
+    name = models.CharField(max_length=250)
+    history = models.CharField(max_length=250)
+    dad = models.CharField(max_length=250)
+    mum = models.CharField(max_length=250)
+    
+    def __str__(self):
+        return self.name
+
+class Announcement(models.Model):
+    item = models.CharField(max_length=1500)
+    date = models.DateField()
+    
+    def __str__(self):
+        return self.item
+    
+class Member(models.Model):
+    family = models.ForeignKey(Family, on_delete=models.RESTRICT)
+    name = models.CharField(max_length=250)
+    contact = models.CharField(max_length=10)
+    
+    def __str__(self):
+        return self.name
