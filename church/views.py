444d1881ef90c57f133c270d7b4b78fdb8f25944
paminus-kingori <pkmurungi.pm@gmail.com>
2022-03-25 09:21:01 +0300

    commit

--- church/views.py
+++ church/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Department, Family, Announcement, Member, Event
+from django.utils.translation import gettext_lazy as _
+
 # Create your views here.
 
 def home(request):
