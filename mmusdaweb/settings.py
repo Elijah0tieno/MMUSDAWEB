990fbb1b3ef95bc0744c3bfe14664e08a21e9c60
Elijah0tieno <eliasotieno99@gmail.com>
2022-08-28 22:34:22 +0300

    final

--- mmusdaweb/settings.py
+++ mmusdaweb/settings.py
@@ -65,10 +65,21 @@ WSGI_APPLICATION = 'mmusdaweb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': str(BASE_DIR / 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(BASE_DIR / 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mmusda',
+        'USER': 'postgres',
+        'PASSWORD': '1/1/1999',
+        'HOST': 'localhost',
+        'PORT': '5432',     
     }
 }
 
