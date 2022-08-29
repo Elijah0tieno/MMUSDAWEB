554f837588ae6eb7dc7b3fa76b97250e206518b0
Elijah0tieno <eliasotieno99@gmail.com>
2022-08-29 12:46:56 +0300

    final

--- mmusdaweb/settings.py
+++ mmusdaweb/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-o34kjnpf$s!c-s#2xxcbv9n8u@auiwr#mz7+k@^*e1ttbol8g('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
