8fa8f06c16a3c75eedcecba6e14895f1fdeb86b7
Elijah0tieno <eliasotieno99@gmail.com>
2022-09-04 20:49:52 +0300

    final

--- mmusdaweb/urls.py
+++ mmusdaweb/urls.py
@@ -17,12 +17,16 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('church.urls', namespace='church'))
     
+    
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+else:
+    urlpatterns += staticfiles_urlpatterns()
